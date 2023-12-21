ad38a80583a901bef0e672c61eda70aa9e2f6dc3
Shashank <86043866+Shashank-saurabh123@users.noreply.github.com>
2023-12-21 09:45:43 +0000

    01_basic,let, var ,const

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId =14453;
+let accountEmail ="saurabh123@gmail.com";
+var accountPassword ="123";
+accountCity="Bihar";
+let city
+//accountId=2 // not allowed
+accountEmail="shashank@gmail.com";
+accountPassword="212223";
+accountCity="Warangal";
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,city]);
+
