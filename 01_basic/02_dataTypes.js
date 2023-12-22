7f2915475eed9c409ddeb9d778a564f518437b26
Shashank <86043866+Shashank-saurabh123@users.noreply.github.com>
2023-12-22 05:29:59 +0000

    Datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";// it treats all JS code as newer version
+//alert(5+8)  //  we are using node js not browser ,it will show an error
+
+// console.log(3
+//     +
+//     8);// code readabilty should be prortiseed
+
+    let name="Shashank"
+    let age= 18
+    let isLoggedIn=false
+    let state;
+    //number=>  range is around 2 to power of 53
+    // bigint => used for large integer,majourly used in trading and stock markets
+    // string=> " "
+    // boolean= true/false
+    // null(intresting)==> standalone value     { zero temperature ka example}
+    // undefined=> value is not assigned yet now
+    // symbol => unique behaviour (mainly in react)
+
+    //object  --> important
+    console.log(typeof undefined);// its output will be "undefined" because it is a type in js
+    console.log(typeof null); //  its output will be object
