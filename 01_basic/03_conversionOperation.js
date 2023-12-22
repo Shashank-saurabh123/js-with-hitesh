32ed7f676e8222b7d12a30981637c16f2c8e8493
Shashank <86043866+Shashank-saurabh123@users.noreply.github.com>
2023-12-22 07:37:15 +0000

    done with Conversions

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -23,4 +23,32 @@ let booleanIsLoggedIn= Boolean(isLoggedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber);
-console.log( typeof (stringNumber));
+//console.log( typeof (stringNumber));
+
+// **************** Operations **********************
+
+let value=3;
+let negvalue = -value;
+//console.log(negvalue);
+
+//console.log(2**3);// double start used as a power function
+let str1="shashank"
+let str2="saurabh"
+let str3= str1+str2;
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);//  if string is in start then output is 122
+// console.log(1+2+"2");// if string is in last then first it will compute mathematically and then it  converts to 32
+// console.log(+true);// +true =>1(converting in to num ,one of the conversion technique but it wont motivated) but true+=> not expected it will throw an error
+// console.log(+"")// it will give 0,but not prefered
+
+let num1,num2,num3;
+num1= num2 = num3 = 2+2
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter)
+// done with conversions
+
