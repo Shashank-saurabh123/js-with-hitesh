42494a2b337ccef3b6dc33d4b29b44a8406e4f8a
Shashank <86043866+Shashank-saurabh123@users.noreply.github.com>
2023-12-22 06:48:58 +0000

    Conversion of Datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+ let score=undefined;
+
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+//  let valueInNumber =Number(score);
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+//* */ for converting you have to start with capital letter ( do Remember)
+//"33"=>33
+//"33abch"=>NaN {not a number}
+//true => 1
+// false=>0
+
+let isLoggedIn="Shashank";
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//1 =>true; 0=> false;
+//""=>false
+//" Sahshank" =>true
+
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log( typeof (stringNumber));
